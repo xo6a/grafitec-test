46ec0ceb99455af308efba7653007f0270a232e1
Letuev Vasiliy <lvs@htc-cs.ru>
2017-10-16 10:48:28 +0400

    [update] HarmonicProgression

---
+++ lib/ProgressionTester/progressions/HarmonicProgression.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ProgressionTester\progressions;
+
+use ProgressionTester\components\AbstractProgression;
+
+class HarmonicProgression extends AbstractProgression
+{
+    protected $name = 'Гармоническая прогрессия';
+
+    protected function getDelta($input, $i)
+    {
+        if (!$this->isSetRange($input, $i, $i + 1))
+            return null;
+        return $input[$i] - $input[$i + 1];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function prepareInput($input)
+    {
+        $newInput = [];
+        foreach ($input as $item) {
+            if ($this->getFractionDenominator($item) === null)
+                throw new \Exception($item . ' is not fraction');
+            $newInput[] = $this->getFractionDenominator($item);
+        }
+        return $newInput;
+    }
+
+    /**
+     * Получить знаменатель
+     * @param $fraction string
+     * @return null|int
+     */
+    protected function getFractionDenominator($fraction)
+    {
+        $fraction = explode('/', $fraction);
+        if (!isset($fraction[1]))
+            return null;
+        return $fraction[1];
+    }
+}
--- test.php
+++ test.php
@@ -9,7 +9,7 @@ $app->addProgressionAll();
 try {
     $app->log('Arithmetic Progression');
     $progName = 'ArithmeticProgression';
-    $results = $app->dropResult();
+    $app->dropResult();
     //success
     $args = '1,2,3,4,5';
     runTest($app, $args, $progName, true);
@@ -19,12 +19,24 @@ try {
 
     $app->log('Geometric Progression');
     $progName = 'GeometricProgression';
-    $results = $app->dropResult();
+    $app->dropResult();
     //success
     $args = '1,2,4,8,16';
     runTest($app, $args, $progName, true);
     //fail
     $args = '1,2,3,4,5';
+    runTest($app, $args, $progName, false);
+
+    $app->log('Harmonic Progression');
+    $progName = 'HarmonicProgression';
+    $app->dropResult();
+    //success
+    $args = '1/2,1/4,1/6,1/8';
+    runTest($app, $args, $progName, true);
+    //fail
+    $args = '1,2,3,4,5';
+    runTest($app, $args, $progName, false);
+    $args = '1/2,1/5,1/6,1/8';
     runTest($app, $args, $progName, false);
 } catch (\Exception $e) {
     $app->log('Error: ' . $e->getMessage());
@@ -42,9 +54,10 @@ function runTest($app, $args, $progName, $expectedResult)
     $app->test($progName);
     $result = $app->getResult();
     $result = $result[$progName];
+    $expected = ($expectedResult) ? 'true' : 'false';
     if ($result['result'] === $expectedResult) {
-        $app->log("[success] test string = $args");
+        $app->log("[success] test string = $args | waiting for " . $expected);
     } else {
-        $app->log("[fail] test string = $args");
+        $app->log("[fail] test string = $args | waiting for " . $expected);
     }
 }
